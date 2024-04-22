873c1e1f2c0643b06d8c0606ca5bbf62a64e36ff
omega-jean <jeanalex1405@gmail.com>
2024-04-22 20:53:58 +0000

    fix code in fonction _getline

--- _getline.c
+++ _getline.c
@@ -10,11 +10,6 @@ char *_getline(void)
 	size_t size = 0;
 	ssize_t read_line;
 
-	/*checks if the standard input is connected to a terminal */
-	if (isatty(STDIN_FILENO))
-	{
-		prompt();/*if its good, display the prompt*/
-	}
 
 	read_line = getline(&line, &size, stdin);/*read input data of line & size*/
 
@@ -22,6 +17,11 @@ char *_getline(void)
 	{
 		free(line);
 		return (0);
+	}
+
+	if (line[read_line - 1] == '\n')
+	{
+		line[read_line - 1] = '\0';
 	}
 	return (line);
 }
