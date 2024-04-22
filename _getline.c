0526c470b160aa6819191985fa10d55f24f6cbcf
omega-jean <jeanalex1405@gmail.com>
2024-04-22 12:44:57 +0000

    file retrieved from Adry branch

---
+++ _getline.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ * _getline - function tu read the line after the prompt
+ * Return: the line.
+*/
+
+char _getline(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t read_line;
+
+	/*checks if the standard input is connected to a terminal */
+	if (isatty(STDIN_FILENO))
+	{
+		prompt();/*if its good, display the prompt*/
+	}
+
+	read_line = getline(&line, &size, stdin);/*read input data of line & size*/
+
+	if (read_line <= 0)
+	{
+		free(line);
+		return (0);
+	}
+	return (line);
+}
