cc3e498b3a47daaa635ddaf07a23a45f6f128551
omega-jean <jeanalex1405@gmail.com>
2024-04-23 08:15:10 +0000

    Fix betty in fontion _execute

--- _execute.c
+++ _execute.c
@@ -1,41 +1,41 @@
 #include "shell.h"
 /**
  * _execute - fonction that execute the line after the prompt
- * @token: pointer array
+ * @command: pointer array
  * Return: 0
 */
 
 int _execute(char *command)
 {
-        int chld;
+	int chld;
 	char *args[] = {"/bin/ls", NULL};
 	pid_t pid = fork();
 
-                if (pid == -1)
-                {
-                        perror("fork error");/*creation failed*/
-                        exit(EXIT_FAILURE);
-                }
-                if (pid == 0)
+	if (pid == -1)
+	{
+		perror("fork error");/*creation failed*/
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if (strcmp(command, "/bin/ls") == 0)
 		{
-			if (strcmp(command, "/bin/ls") == 0)
+			if (execve("/bin/ls", args, NULL) == -1)
 			{
-				if (execve("/bin/ls", args, NULL) == -1)
-				{
-                        		perror("./hsh");
-                        		exit(EXIT_FAILURE);
-				}
+				perror("./hsh");
+				exit(EXIT_FAILURE);
 			}
-			else
-			{
-				printf("./hsh 1: %s: command not found\n", command);
-            			exit(EXIT_FAILURE);
-			}
-                }
-                else
-                {
-                        waitpid(pid, &chld, 0);/*Wait for the child process to finish*/
-                }
-        return (0);
+		}
+		else
+		{
+			printf("./hsh 1: %s: command not found\n", command);
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &chld, 0);/*Wait for the child process to finish*/
+	}
+	return (0);
 }
 
