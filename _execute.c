472992889da8bb192052713c6938876a06b90fa7
omega-jean <jeanalex1405@gmail.com>
2024-04-23 08:19:51 +0000

    Adding the files of the Jean-Alexandre branch to main

---
+++ _execute.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+/**
+ * _execute - fonction that execute the line after the prompt
+ * @command: pointer array
+ * Return: 0
+*/
+
+int _execute(char *command)
+{
+	int chld;
+	char *args[] = {"/bin/ls", NULL};
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork error");/*creation failed*/
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		if (strcmp(command, "/bin/ls") == 0)
+		{
+			if (execve("/bin/ls", args, NULL) == -1)
+			{
+				perror("./hsh");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			printf("./hsh 1: %s: command not found\n", command);
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &chld, 0);/*Wait for the child process to finish*/
+	}
+	return (0);
+}
+
---
+++ _getline.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ * _getline - function tu read the line after the prompt
+ * Return: the line.
+*/
+
+char *_getline(void)
+{
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t read_line;
+
+
+	read_line = getline(&line, &size, stdin);/*read input data of line & size*/
+
+	if (read_line <= 0)
+	{
+		free(line);
+		return (0);
+	}
+
+	if (line[read_line - 1] == '\n')
+	{
+		line[read_line - 1] = '\0';
+	}
+	return (line);
+}
